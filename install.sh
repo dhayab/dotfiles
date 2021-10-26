c3e8b23bcb35736900a8ee9cb76ddc4687c4c0fc
Dhaya <154633+dhayab@users.noreply.github.com>
2021-10-26 14:27:57 +0200

    fix: add homebrew shell env after install

--- install.sh
+++ install.sh
@@ -203,6 +203,7 @@ brew_bin=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
   action "installing homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
   if [[ $? != 0 ]]; then
     error "unable to install homebrew, script $0 abort!"
     exit 2
